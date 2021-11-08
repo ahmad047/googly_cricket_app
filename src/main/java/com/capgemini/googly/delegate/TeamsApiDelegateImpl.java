c86885cb336ecfcc25f539a1842b65650967bf8f
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-11-08 10:22:02 +0000

    mysql connection set up

--- src/main/java/com/capgemini/googly/delegate/TeamsApiDelegateImpl.java
+++ src/main/java/com/capgemini/googly/delegate/TeamsApiDelegateImpl.java
@@ -2,13 +2,18 @@ package com.capgemini.googly.delegate;
 
 import com.capgemini.googly.generated.api.TeamsApiDelegate;
 import com.capgemini.googly.generated.model.Team;
+import com.capgemini.googly.repositories.TeamRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@AllArgsConstructor
 public class TeamsApiDelegateImpl implements TeamsApiDelegate {
+    private final TeamRepository teamRepository;
 
     @Override
     public ResponseEntity<Team> getTeamById(Long teamId) {
@@ -17,6 +22,7 @@ public class TeamsApiDelegateImpl implements TeamsApiDelegate {
 
     @Override
     public ResponseEntity<List<Team>> getTeams() {
-        return TeamsApiDelegate.super.getTeams();
+        List<Team> teams = teamRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(teams);
     }
 }
