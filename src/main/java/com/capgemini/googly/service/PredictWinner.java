e73e617de3810633d3642bbf939fd5736095e0d8
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-11-08 15:45:45 +0000

    incomplete impl of prediction

--- src/main/java/com/capgemini/googly/service/PredictWinner.java
+++ src/main/java/com/capgemini/googly/service/PredictWinner.java
@@ -7,13 +7,21 @@ import com.capgemini.googly.generated.model.Team;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class PredictWinner {
     public PredictionEntity predictWinner(Team teamOne, Team teamTwo){
+
+
         PredictionEntity predictionEntity = new PredictionEntity(teamOne.getId(), teamTwo.getId(), LocalDate.now(),
                 teamOne.getId(), 75D);
-
         return predictionEntity;
     }
+
+//    public HashMap<String, Integer> rankings(String teamOneName, String teamTwoName){
+//        fetch(https://cricket.sportmonks.com/api/v2.0/team-rankings?api_token=TGhymWCwRhpEpQtKtx1XP8ZzQ7CBoHhOMG6G0L6pUQugu8Oev7SfFQCedCYS)
+//    }
 }
