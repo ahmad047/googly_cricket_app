8ecd25c1d092e03984b8142e13f3346550f6fea8
Dupplaw, Robert (Capgemini Flex) <robert.dupplaw@capgemini.com>
2021-11-09 13:37:31 +0000

    Replace ControllerAdvice annotation with RestControllerAdvice
    This means the handler methods don't need to be annotated with ResponseBody.

--- src/main/java/com/capgemini/googly/exception/ResourceNotFoundAdvice.java
+++ src/main/java/com/capgemini/googly/exception/ResourceNotFoundAdvice.java
@@ -1,15 +1,13 @@
 package com.capgemini.googly.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ResourceNotFoundAdvice {
 
-    @ResponseBody
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String resourceNotFoundHandler(ResourceNotFoundException ex) {
